6c380acf043e1688c0703feccc737ff039d70f6c
abdomalouki2 <130778358+abdomalouki2@users.noreply.github.com>
2023-05-09 19:22:29 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,15 +2,15 @@
     'name': 'Fleet Management',
     'version': '1.0',
     'category': 'Fleet',
-    'description': """
-        This module manages vehicles and their maintenance.
-    """,
+    'description': "Aquest modul es de Vehicles",
     'author': 'Abde-Tarik',
     'depends': ['base'],
     'data': [
+        'data/fleet.vehicle.csv'
         'security/ir.model.access.csv',
         'views/fleet_vehicle_views.xml'
     ],
     'installable': True,
-    'auto_install': True,
+    'aplication':True,
+    'auto_install': False,
 }
